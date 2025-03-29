488c2c68b8d6a1775c4e6b6d32a9e530e92cde7d
araf821 <araf.ahmed200@gmail.com>
2025-03-29 17:31:04 -0400

    feat: add HomePage and DashboardLayout components for user authentication and layout structure

---
+++ app/(main)/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayout = (props: Props) => {
+  return (
+    <div className="flex min-h-svh bg-orange-200">
+      {/* sidebar */}
+      <aside className="fixed z-30 h-svh w-80 bg-orange-200 max-lg:hidden"></aside>
+
+      {/* main content */}
+      <main className="mt-4 h-full min-h-svh flex-1 rounded-tl-2xl border bg-white lg:ml-80">
+        {props.children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
