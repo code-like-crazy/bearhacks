36223d07651d2710564472c82288e4c6a20f8e96
araf821 <araf.ahmed200@gmail.com>
2025-03-29 02:51:02 -0400

    feat: add user retrieval functions by ID and email/password

--- services/user.ts
+++ services/user.ts
@@ -1,9 +1,22 @@
 "server only";
 
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 import { db } from "@/lib/db";
 import { usersTable } from "@/lib/db/schema";
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, id));
+    return user[0];
+  } catch (error) {
+    console.error("Error fetching user in getUserById function: ", error);
+    return null;
+  }
+};
 
 export const getUserByEmail = async (email: string) => {
   try {
@@ -14,6 +27,30 @@ export const getUserByEmail = async (email: string) => {
     return user[0];
   } catch (error) {
     console.error("Error fetching user in getUserByEmail function: ", error);
+    return null;
+  }
+};
+
+export const getUserByEmailAndPassword = async (
+  email: string,
+  password: string,
+) => {
+  try {
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(
+        and(
+          eq(usersTable.email, email),
+          eq(usersTable.hashedPassword, password),
+        ),
+      );
+    return user[0];
+  } catch (error) {
+    console.error(
+      "Error fetching user in getUserByEmailAndPassword function: ",
+      error,
+    );
     return null;
   }
 };
