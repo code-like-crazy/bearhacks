8597db2de07e7ff64f976d3dd918a7dbbc863ac1
araf821 <araf.ahmed200@gmail.com>
2025-03-29 19:07:28 -0400

    feat: remove HomePage and DashboardLayout components, integrate layout into MainLayout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,17 @@ type Props = {
 };
 
 const MainLayout = (props: Props) => {
-  return <div>{props.children}</div>;
+  return (
+    <div className="flex min-h-svh bg-gray-50">
+      {/* sidebar */}
+      <aside className="fixed z-30 h-svh w-80 bg-gray-50 max-lg:hidden"></aside>
+
+      {/* main content */}
+      <main className="mt-4 flex h-full min-h-[calc(100svh-1rem)] flex-1 rounded-tl-2xl border bg-white lg:ml-80">
+        {props.children}
+      </main>
+    </div>
+  );
 };
 
 export default MainLayout;
